bd79646e0d5a501191a6b59dcc4f4191edfd5b66
Flashowing <980713832@qq.com>
2022-10-21 00:24:24 +0800

    去除版权信息

--- src/cesium/Gy.ts
+++ src/cesium/Gy.ts
@@ -2,10 +2,13 @@ import Camera from "../scene/camera/Camera";
 
 export default class Gy {
   camera: any = null;
+
   constructor(viewer: any) {
     if (!viewer) {
       throw Error("viewer can not be null");
     }
+    // 去除版权信息
+    viewer._cesiumWidget._creditContainer.style.display = "none";
     this.camera = new Camera(viewer);
   }
 }
